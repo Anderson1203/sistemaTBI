89f7a810a7e8f0908d5cd5bf197dc597b47225cb
unknown <ander_hhh2@hotmail.es>
2018-10-02 12:55:41 -0600

    sccccccccccccc

--- resources/views/sistema/factura/create.blade.php
+++ resources/views/sistema/factura/create.blade.php
@@ -19,7 +19,7 @@
      		<div class="form-group">
                     <label for="cliente">Cliente</label>
                        
-                    <select name="IdCliente" class="form-control" id="selec-client" onclick="true">
+                    <select name="IdCliente" disabled="disabled" class="form-control" id="selec-client" onclick="true">
                          @foreach($clientes as $clien)
 
                              <option value="{{$clien->idClientes}}">{{$clien->Nombre}}</option>
@@ -29,12 +29,13 @@
 
                <div class="form-group">
                     <label for="zona">Zona</label>
-                    <select name="idZona" class="form-control" id="selec-zon">
+                    <select name="idZona" class="form-control" disabled="disabled" id="selec-zon">
                       @foreach($zonas as $zones)
-                          <option value="{{$zones->idZona}}">{{$zones->Nombre}}</option>
+                          <option value="{{$zones->idZona}}" >{{$zones->Nombre}}</option>
                       @endforeach
                     </select>
                </div>
+
 
                <div class="form-group">
                     <label for="formas">Forma de Pago</label>
@@ -45,20 +46,25 @@
                     </select>
                </div>
 
-     		  <div class="form-group">
-                    <label for="fecp">Fecha de Pago</label>
-                    <input type="date" name="FechaPago" class="form-control" placeholder="FechaPago..." >
-               </div>
-
-
                <div class="form-group">
                     <label for="fece">Fecha de Emision</label>
                     <input type="date" name="FechaEmision" class="form-control" placeholder="FechaEmision..." value="<?php echo date("Y-m-d");?>" >
                </div>
+               <script>
+                 var fecha = new Date($('#fecp').val());
+                 var dias = 2; // Número de días a agregar
+                 fecha.setDate(fecha.getDate() + dias);
+                 console.info(fecha)
+               </script>
+
+          <div class="form-group">
+                    <label for="fecp">Fecha de Pago</label>
+                    <input type="date" name="FechaPago" class="form-control" placeholder="FechaPago..." id="fecp" value="1999-12-1">
+               </div>
 
                <div class="form-group">
                     <label for="fecv">Fecha de Vencimiento</label>
-                    <input type="date" name="FechaVenci" class="form-control" placeholder="FechaVenci..." >
+                    <input type="date" name="FechaVenci" class="form-control" placeholder="FechaVenci..." value="">
                </div>
 
      		<div class="form-group">
@@ -80,7 +86,7 @@
                <div class="form-group">
                     <label for="total">Total</label>
                     @foreach($planes as $pl)
-                    <input value="{{$pl->Precio}}" type="text" name="Total" class="form-control" placeholder="Total...">
+                    <input value="{{$pl->Precio}}" type="number" name="Total" class="form-control" placeholder="Total...">
                     @endforeach
                </div>
 
